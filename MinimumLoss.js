eef92b1df6193a4293b32279f278134558a0384e
Aniket_Mahindrakar <aniketmm98@gmail.com>
2019-04-24 18:13:00 +0530

    Added Minimum Loss program

---
+++ MinimumLoss.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumLoss function below.
+function minimumLoss(price) {
+    var len = price.length;
+    var t = Infinity;
+
+    for (var i = 0; i < len - 1; i++){
+        var l = price.slice(i + 1, len).filter((e) => e < price[i]);
+
+        var m = Math.max(...l);
+
+        var s = price[i] - m;
+
+        if (s < t) {
+            t = s;
+        }
+    }
+
+    return t;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const price = readLine().split(' ').map(priceTemp => parseInt(priceTemp, 10));
+
+    let result = minimumLoss(price);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
