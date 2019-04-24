e7d7ea2fd991c971a6e89cc826c54781b697e780
Aniket_Mahindrakar <aniketmm98@gmail.com>
2019-04-24 18:14:54 +0530

    Added Sherlock & Array program

---
+++ SherlockAndArray.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the balancedSums function below.
+function balancedSums(arr) {
+    var temp = arr.map((e, i) => arr.slice(0, i).reduce(function (a, b) { return a + b; }, 0) == arr.slice(i + 1, arr.length).reduce(function (a, b) { return a + b; }, 0));
+
+    var b = temp.some((e) => e==true);
+
+    if (b) {
+        return 'YES';
+    } else {
+        return 'NO';
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const T = parseInt(readLine().trim(), 10);
+
+    for (let TItr = 0; TItr < T; TItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+        const result = balancedSums(arr);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
