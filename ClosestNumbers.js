ee0cc3b246b18cfbe1e6d277c515cfe0ffd85a22
Aniket_Mahindrakar <aniketmm98@gmail.com>
2019-04-24 11:44:21 +0530

    Added Closest Numbers

---
+++ ClosestNumbers.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the closestNumbers function below.
+function closestNumbers(arr) {
+    arr.sort((a, b) => a - b);
+    var temp = [];
+    var ans = [];
+
+    for (var i = 1; i < arr.length; i++){
+        temp.push(arr[i] - arr[i-1]);
+    }
+
+    var ind = getAllIndexes(temp, Math.min(...temp));
+
+    for (var i in ind) {
+        ans.push(arr[ind[i]]);
+        ans.push(arr[ind[i] + 1]);
+    }
+
+    return ans;
+}
+
+function getAllIndexes(arr, val) {
+    var indexes = [], i = -1;
+    while ((i = arr.indexOf(val, i + 1)) != -1) {
+        indexes.push(i);
+    }
+    return indexes;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = closestNumbers(arr);
+
+    ws.write(result.join(" ") + "\n");
+
+    ws.end();
+}
